82e29885ace2d9fbc73097b7714baad1e3f0cb61
Daniel CAUNE <daniel.caune@gmail.com>
2024-08-29 17:10:35 +0700

    fix: return a timestamp instead of date

--- src/thelittlehackers/utils/string_utils.py
+++ src/thelittlehackers/utils/string_utils.py
@@ -189,7 +189,7 @@ def string_to_timestamp(
     try:
         if not any_utils.is_empty_or_none(value):
             return value if isinstance(value, datetime) \
-                else dateutil.parser.parse(value).date()
+                else dateutil.parser.parse(value).timestamp()
     except ValueError as error:
         if strict:
             raise error
