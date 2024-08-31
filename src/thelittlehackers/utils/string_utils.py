87d19cc7fd551148f39e29052d31097663707a6a
Daniel CAUNE <daniel.caune@gmail.com>
2024-08-31 22:44:31 +0700

    feat: support new converters

--- src/thelittlehackers/utils/string_utils.py
+++ src/thelittlehackers/utils/string_utils.py
@@ -23,22 +23,32 @@
 
 from __future__ import annotations
 
+import datetime
 import dateutil.parser
-from datetime import date
-from datetime import datetime
+import re
+import string
+from enum import Enum
+from typing import AnyStr
+from typing import Callable
+from typing import Type
 from uuid import UUID
 
+from thelittlehackers.constant import regex
 from thelittlehackers.constant.data_type import DataType
 from thelittlehackers.model.locale import Locale
+from thelittlehackers.model.version import Version
 from thelittlehackers.utils import any_utils
-from thelittlehackers.utils.any_utils import is_empty_or_none
+
+
+REGEX_IPV4: re.Pattern[AnyStr] = re.compile(regex.REGEX_PATTERN_IPV4)
+REGEX_MAC_ADDRESS: re.Pattern[AnyStr] = re.compile(regex.REGEX_PATTERN_MAC_ADDRESS)
 
 
 def string_to_boolean(
         value: bool | str | None,
         strict: bool = False,
         default_value: bool = False
-):
+) -> bool:
     """
     Convert a string representation of a boolean value to its
     corresponding boolean.
@@ -70,13 +80,10 @@ def string_to_boolean(
     :raise ValueError: If ``strict`` is ```True``` and `value` does not
         represent a valid boolean.
     """
-    if is_empty_or_none(value) and default_value is not None:
-        return default_value
-
     if isinstance(value, bool):
         return value
 
-    if isinstance(value, str) and value is not None:
+    if isinstance(value, str):
         value = value.lower()
 
     if value in ('yes', 'y', 'true', 't', '1'):
@@ -92,9 +99,9 @@ def string_to_boolean(
 
 
 def string_to_date(
-        value: str | date | None = None,
+        value: str | datetime.date | None = None,
         strict: bool = True
-) -> date | None:
+) -> datetime.date | None:
     """
     Convert a string representation of a date value to its corresponding
     date.
@@ -108,7 +115,7 @@ def string_to_date(
         invalid date strings.
 
 
-    :return: The date value corresponding to the string.
+    :return: The date corresponding to the string.
 
 
     :raise OverflowError: Raised if the parsed date exceeds the largest
@@ -118,14 +125,118 @@ def string_to_date(
         represent a valid date or the string format is unknown.
     """
     try:
-        if not any_utils.is_empty_or_none(value):
-            return value if isinstance(value, date) \
-                else dateutil.parser.parse(value).date()
+        date = None if any_utils.is_empty_or_none(value) \
+            else value if isinstance(value, datetime.date) \
+            else dateutil.parser.parse(value).date()
     except ValueError as error:
         if strict:
             raise error
 
-    return None
+    if date is None:
+        if strict:
+            f"The string \"{value}\" does not represent a date"
+
+    return date
+
+
+def string_to_decimal(
+        value: str | float | None,
+        strict: bool = True
+) -> float | None:
+    """
+    Convert a string representation of a floating number value to its
+    corresponding floating number.
+
+
+    :param value: The input to be converted into an floating number.
+
+    :param strict: A boolean flag indicating whether to enforce strict
+        validation.  If ``True``, a ``ValueError`` is raised for invalid
+        floating number strings.  If ``False``, the function returns
+        ``None`` for invalid floating number strings.
+
+
+    :return: The floating number value corresponding to the string.
+
+
+    :raise ValueError: If ``strict`` is ```True``` and ``value`` does not
+        represent a valid floating number.
+    """
+    try:
+        decimal = None if any_utils.is_empty_or_none(value) \
+            else value if isinstance(value, float) \
+            else float(value)
+    except ValueError as error:
+        if strict:
+            raise error
+
+    if decimal is None:
+        if strict:
+            f"The string \"{value}\" does not represent a floating number"
+
+    return decimal
+
+
+def string_to_enumeration_member(
+        value: str | Enum | None,
+        enumeration: Type[Enum],
+        strict: bool = True,
+        default_value: Enum | None = None
+) -> Enum | None:
+    """
+    Convert a string representation of an enumeration member to its
+    corresponding enumeration member.
+
+
+    :param value: The input value to convert.  It can be a string
+        representing the name of an enumeration member, an existing
+        enumeration member, or ``None``.
+
+    :param enumeration: The enumeration class to which the value should
+        belong.
+
+    :param strict: A boolean flag indicating whether to enforce strict
+        validation.  If ``True``, a ``ValueError`` is raised for invalid
+        enumeration strings.  If ``False``, the function returns ``None``
+        for invalid enumeration strings.
+
+    :param default_value: The default enumeration member to return if
+        ``value`` is ``None`` or an empty string.  Defaults to ``None``.
+
+
+    :return: The corresponding enumeration member if conversion succeeds,
+        or ``None`` if ``strict`` is ``False`` and conversion fails.
+
+
+    :raise ValueError: If ``enumeration`` is not a subclass of ``Enum``.
+
+    :raise ValueError: If ``value`` is a string that does not match any
+        member of ``enumeration`` and ``strict`` is ``True``.
+    """
+    if not isinstance(enumeration, Enum):
+        raise ValueError(
+            "The argument \"enumeration\" must be a subclass of Enum.  Received "
+            f"\"{enumeration.__name__}\" instead."
+        )
+
+    try:
+        enumeration_member = None if any_utils.is_empty_or_none(value) \
+            else value if value in enumeration \
+            else enumeration(value)
+    except ValueError as error:
+        if strict:
+            raise error
+
+    if enumeration_member is None:
+        if default_value:
+            enumeration_member = enumeration(default_value)
+        elif strict:
+            raise ValueError(
+                f"The string \"{value}\" does not represent a member of the "
+                f"enumeration \"{enumeration.__name__}\""
+            )
+
+    return enumeration_member
 
 
 def string_to_integer(
@@ -152,13 +263,142 @@ def string_to_integer(
         represent a valid integer.
     """
     try:
-        if not any_utils.is_empty_or_none(value):
-            return value if isinstance(value, int) else int(value)
+        integer = None if any_utils.is_empty_or_none(value) \
+            else value if isinstance(value, int) \
+            else int(value)
     except ValueError as error:
         if strict:
             raise error
 
-    return None
+    if integer is None:
+        if strict:
+            f"The string \"{value}\" does not represent an integer"
+
+    return integer
+
+
+def string_to_ipv4(
+        value: str | None,
+        strict: bool = True
+) -> tuple[int, int, int, int] | None:
+    """
+    Convert a string representation of an IPv4 address into a tuple of
+    four integers.
+
+    Examples:
+    >>> string_to_ipv4('192.168.1.1')
+    (192, 168, 1, 1)
+
+    >>> string_to_ipv4('256.168.1.1')
+    ValueError: The IPv4 "256.168.1.1" has invalid byte(s)
+
+    >>> string_to_ipv4(None)
+    ValueError: The specified string "None" does not represent an IPv4
+
+    >>> string_to_ipv4(None, strict=False)
+    None
+
+
+    :param value: The input to be converted into an IPv4 address.  It
+        should be in the format `'X.X.X.X'`, where X is an integer between
+        ``0`` and ``255``.   If ``value`` is ``None`` or an empty string,
+         the function will return ``None`` unless ``strict`` is set to
+         ``True``.
+
+    :param strict: If ``True``, the function will raise a ``ValueError``
+        if the string is ``None``, empty, or does not represent a valid
+        IPv4 address.  If ``False``, the function will return ``None`` fpr
+        such cases.
+
+
+    :return: A tuple of four integers representing the IPv4 address, where
+        each integer corresponds to one byte of the address (e.g.,
+        ``(192, 168, 1, 1)``).  If ``value`` is ``None``, empty, or
+        invalid (and ``strict`` is ``False``), the function returns ``None``.
+
+
+    :raise ValueError: If the input string does not represent a valid IPv4
+        address and ``strict`` is ``True``.  This includes cases where the
+        string is ``None``, empty, or has invalid bytes (e.g., values not
+        in the range ``0-255``).
+    """
+    match = None if any_utils.is_empty_or_none(value) else REGEX_IPV4.match(value)
+    ipv4_address = match and match.group(0).split('.')
+
+    if ipv4_address is None:
+        if strict:
+            raise ValueError(
+                f"The string \"{value}\" does not represent a IPV4 address"
+            )
+    else:
+        ipv4_address = tuple(
+            int(component)
+            for component in ipv4_address
+        )
+
+    return ipv4_address
+
+
+def string_to_mac_address(
+        value: str | None,
+        strict: bool = True
+) -> tuple[str, str, str, str, str, str] | None:
+    """
+    Convert a string representation of a Media Access Control (MAC)
+    address into a tuple of hexadecimal strings.
+
+    A MAC address is a unique identifier assigned to a network interface
+    controller (NIC) for communications at the data link layer of a
+    network segment. The standard IEEE 802 format for representing EUI-48
+    addresses is six groups of two hexadecimal digits, separated by
+    hyphens (e.g., ``01-23-45-67-89-AB``). Other common formats include:
+
+    - Six groups separated by colons (e.g., ``01:23:45:67:89:AB``)
+    - Three groups of four hexadecimal digits separated by dots (e.g.,
+      ``0123.4567.89AB``)
+
+    The function validates the input string against these formats and
+    returns the corresponding hexadecimal components.
+
+
+    :param value: A string representation of a MAC address.  If `None`,
+        the function will behave according to the ``strict`` parameter.
+
+    :param strict: If `True`, the function raises a ValueError when the
+        input string does not match a valid MAC address format.  If
+        ``False``, the function returns ``None`` for invalid inputs.
+
+
+    :return: A tuple of six hexadecimal strings ``(hex1, hex2, hex3, hex4,
+        hex5, hex6)`` representing the MAC address, each ranging from
+        ``0x00`` to ``0xFF``.  Return ``None`` if the input is invalid and
+        ``strict`` is set to ``False``.
+    """
+    match = None if any_utils.is_empty_or_none(value) else REGEX_MAC_ADDRESS.match(value)
+    mac_address = match and match.group(0)
+
+    if mac_address is None:
+        if strict:
+            raise ValueError(
+                f"The string \"{value}\" does not represent a MAC address"
+            )
+    else:
+        # Remove separator characters from the MAC address string
+        # representation.
+        mac_address_hexdigits = [
+            c for c in mac_address if c in string.hexdigits
+        ]
+
+        # Split the string into pairs of characters.
+        pairs = [
+            mac_address_hexdigits[i:i + 2]
+            for i in range(0, len(mac_address_hexdigits), 2)
+        ]
+
+        # Convert the list of pairs into a tuple.
+        mac_address = tuple(pairs)
+
+    return mac_address
 
 
 def string_to_locale(
@@ -167,7 +407,7 @@ def string_to_locale(
 ) -> Locale | None:
     """
     Convert a string representation of a locale to its corresponding
-    ``Locale`` instance.
+    ``Locale`` object.
 
 
     :param value: The input to be converted into a locale.  The input
@@ -181,26 +421,83 @@ def string_to_locale(
         of `-`).
 
 
-    :return: The ``Locale`` instance corresponding to the string.
+    :return: The ``Locale`` object corresponding to the string.
 
 
     :raise MalformedLocaleException: If ``locale`` does not represent a
         valid locale.
     """
     try:
-        return None if is_empty_or_none(value) else Locale.from_string(value, strict=strict)
+        locale = None if any_utils.is_empty_or_none(value) \
+            else value if isinstance(value, Locale) \
+            else Locale.from_string(value, strict=strict)
     except Locale.MalformedLocaleException as exception:
         if strict:
             raise exception
 
+    if locale is None:
+        if strict:
+            f"The string \"{value}\" does not represent a locale"
+
+    return locale
+
+
+def string_to_time(
+        value: str | datetime.time | None,
+        strict: bool = True
+) -> datetime.time | None:
+    """
+    Convert a string representation of a time value into a `datetime.time`
+    object.
+
+
+    :param value: The input to be converted into a `datetime.time` object.
+        This can be a string, a ``datetime.time`` object, or ``None``.
+        A string must be given in the format `HH:MM` or `HH:MM:SS`, where:
+
+        - ``HH``: Two-digit hour in 24-hour format (00-23).
+        - ``MM``: Two-digit minute (00-59).
+        - ``SS``: Two-digit second (00-59).
+
+    :param strict: A boolean flag indicating whether to enforce strict
+        validation.  If ``True``, a ``ValueError`` is raised for invalid
+        time strings.  If ``False``, the function returns ``None`` for
+        invalid time strings.
+
+
+    :return: A ``datetime.time`` object representing the time specified by
+        the input string, or ``None`` if the input is ``None`` or an
+        invalid string in non-strict mode.
+
+
+    :raise ValueError: If ``strict`` is ```True``` and ``value`` does not
+        represent a valid time in the expected format.
+    """
+    try:
+        time = None if any_utils.is_empty_or_none(value) \
+            else value if isinstance(value, datetime.time) \
+            else datetime.datetime.strptime(value, '%H:%M:%S').time()
+    except ValueError:
+        try:
+            return datetime.datetime.strptime(value, '%H:%M').time()
+        except ValueError as error:
+            if strict:
+                raise error
+
+    if time is None:
+        if strict:
+            f"The string \"{value}\" does not represent a time"
+
+    return time
+
 
 def string_to_timestamp(
-        value: str | datetime | None,
+        value: str | datetime.datetime | None,
         strict: bool = True
-) -> datetime | None:
+) -> datetime.datetime | None:
     """
-    Convert a string representation of a date/time value to its
-    corresponding date/time.
+    Convert a string representation of a date/time value into a
+    ``datetime.datetime`` object.
 
 
     :param value: The input to be converted into a date/time.
@@ -221,14 +518,58 @@ def string_to_timestamp(
         represent a valid date/time or the string format is unknown.
     """
     try:
-        if not any_utils.is_empty_or_none(value):
-            return value if isinstance(value, datetime) \
-                else dateutil.parser.isoparse(value)
+        timestamp = None if any_utils.is_empty_or_none(value) \
+            else value if isinstance(value, datetime.datetime) \
+            else dateutil.parser.isoparse(value)
     except ValueError as error:
         if strict:
             raise error
 
-    return None
+    if timestamp is None:
+        if strict:
+            f"The string \"{value}\" does not represent a timestamp"
+
+    return timestamp
+
+
+def string_to_version(
+        value: str | Version | None,
+        strict: bool = True
+) -> Version | None:
+    """
+    Convert a string representation of a version value to its
+    corresponding version.
+
+
+    :param value: The input to be converted into a version.
+
+    :param strict: A boolean flag indicating whether to enforce strict
+        validation.  If ``True``, a ``ValueError`` is raised for invalid
+        version strings complying with Semantic Version 2.  If ``False``,
+        the function returns ``None`` for invalid version strings.
+
+
+    :return: The ``Version`` instance corresponding to the string.
+
+
+    :raise ValueError: If ``strict`` is ```True``` and ``value`` does not
+        represent a valid version or the string doesn't comply to Semantic
+        Version 2.
+    """
+    try:
+        version = None if any_utils.is_empty_or_none(value) \
+            else value if isinstance(value, Version) \
+            else Version(value, strict=strict)
+    except ValueError as error:
+        if strict:
+            raise error
+
+    if version is None:
+        if strict:
+            f"The string \"{value}\" does not represent a version"
+
+    return version
+
 
 def string_to_uuid(
         value: str | UUID | None,
@@ -261,30 +602,34 @@ def string_to_uuid(
         valid UUID.
     """
     try:
-        if not any_utils.is_empty_or_none(value):
-            return value if isinstance(value, UUID) \
-                else UUID(value)
+        uuid = None if any_utils.is_empty_or_none(value) \
+            else value if isinstance(value, UUID) \
+            else UUID(value)
     except ValueError as error:
         if strict:
             raise error
 
-    return None
+    if uuid is None:
+        if strict:
+            f"The string \"{value}\" does not represent a UUID"
+
+    return uuid
 
 
-DATA_TYPE_CONVERTERS = {
-    DataType.BOOLEAN: string_to_boolean(),
-    DataType.DATE: string_to_date(),
-    DataType.DECIMAL: __convert_to_decimal,
-    DataType.EMAIL_ADDRESS: __convert_to_email_address,
-    DataType.ENUMERATION: __convert_to_enumeration,
+DATA_TYPE_CONVERTERS: {DataType, Callable} = {
+    DataType.BOOLEAN: string_to_boolean,
+    DataType.DATE: string_to_date,
+    DataType.DECIMAL: string_to_decimal,
+    # DataType.EMAIL_ADDRESS: __convert_to_email_address,
+    DataType.ENUMERATION: string_to_enumeration_member,
     DataType.INTEGER: string_to_integer,
-    DataType.IPV4: __convert_to_ipv4,
-    DataType.LIST: __convert_to_list,
+    DataType.IPV4: string_to_ipv4,
+    # DataType.LIST: __convert_to_list,
     DataType.LOCALE: string_to_locale,
-    DataType.MAC_ADDRESS: __convert_to_macaddr,
-    DataType.STRING: __convert_to_string,
-    DataType.TIME: __convert_to_time,
-    DataType.TIMESTAMP: string_to_timestamp(),
-    DataType.UUID: string_to_uuid(),
-    DataType.VERSION: __convert_to_version,
+    DataType.MAC_ADDRESS: string_to_mac_address,
+    # DataType.STRING: __convert_to_string,
+    DataType.TIME: string_to_time,
+    DataType.TIMESTAMP: string_to_timestamp,
+    DataType.UUID: string_to_uuid,
+    DataType.VERSION: string_to_version,
 }
